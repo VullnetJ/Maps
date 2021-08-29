05ab73dc5e1d30b0cd3f053ad1b0e3760ddb706b
VullnetJ <v.jata24@gmail.com>
2021-08-29 16:51:46 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 60.200692,
+          longitude: 24.934302,
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0,0221
+        }}>
+        <Marker coordinate={{
+        latitude: 60.200692,
+        longitude: 24.934302
+        }}/>
+      </MapView>
+
+
     </View>
   );
 }
